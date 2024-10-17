4efbcdee8fb1ce5fe10fc71e14a8898fe46c99f4
kubraaydogdu <kubraaydgdu2525@gmail.com>
2024-10-17 16:52:23 +0300

    17-Ekim/3-command

---
+++ cypress/e2e/3-lessons/baslangic/o1-ilk-test.cy.js
@@ -0,0 +1,10 @@
+describe("İlk Testim", () => {
+  it("true,true ile esit mi!", () => {
+    expect(true).to.equal(true);
+  });
+
+  it("true,terue ile esit mi!", () => {
+    //intellisense
+    cy.get("").click();
+  });
+});
